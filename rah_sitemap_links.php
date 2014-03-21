feb6d4435537ae2ed8b25dfbe1435e8b082effd0
Jukka Svahn <web@rahinaa.biz>
2014-03-21 13:42:07 +0200

    Programming style.

--- rah_sitemap_links.php
+++ rah_sitemap_links.php
@@ -1,15 +1,24 @@
 <?php
 
-/**
- * Links module for rah_sitemap.
+/*
+ * rah_sitemap_links - Links module for rah_sitemap
+ * https://github.com/gocom/rah_sitemap_links
  *
- * @author  Jukka Svahn
- * @license GNU GPLv2
- * @link    http://rahforum.biz/plugins/rah_sitemap_links
+ * Copyright (C) 2014 Jukka Svahn
  *
- * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
- * Licensed under GNU General Public License version 2
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * This file is part of rah_sitemap_links.
+ *
+ * rah_sitemap_links is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_sitemap_links is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_sitemap_links. If not, see <http://www.gnu.org/licenses/>.
  */
 
 register_callback('rah_sitemap_links_urlset', 'rah_sitemap.urlset');
@@ -25,19 +34,17 @@ register_callback('rah_sitemap_links_urlset', 'rah_sitemap.urlset');
 
 function rah_sitemap_links_urlset($event, $step, $void, $urls)
 {
-	$local = str_replace(array('%', '_'), array('\\%', '\\_'), doSlash(hu));
+    $local = str_replace(array('%', '_'), array('\\%', '\\_'), doSlash(hu));
 
-	$rs = safe_rows_start(
-		'url, date',
-		'txp_link',
-		"category = 'rah_sitemap' or url like '".$local."_%' or url like '/_%'"
-	);
+    $rs = safe_rows_start(
+        'url, date',
+        'txp_link',
+        "category = 'rah_sitemap' or url like '".$local."_%' or url like '/_%'"
+    );
 
-	if ($rs)
-	{
-		while ($a = nextRow($rs))
-		{
-			$urls[$a['url']] = $a['date'];
-		}
-	}
+    if ($rs) {
+        while ($a = nextRow($rs)) {
+            $urls[$a['url']] = $a['date'];
+        }
+    }
 }
